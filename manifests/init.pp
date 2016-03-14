f6520f6fc8e7d4a08f9548a129280f984f099f43
Simon Kurka <dev@simon.kurka.cc>
2016-03-14 12:53:11 +0100

    nets_self paramters added

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 class backbone (
-  Array[String]                     $ip           = $backbone::params::ip,
-  Array[String]                     $ip6          = $backbone::params::ip6,
+  Array[String] $ip         = $backbone::params::ip,
+  Array[String] $ip6        = $backbone::params::ip6,
+  Array[String] $nets_self  = $backbone::params::nets_self,
+  Array[String] $nets_self6 = $backbone::params::nets_self6,
 ) inherits backbone::params {
 
   class { 'backbone::config': }
